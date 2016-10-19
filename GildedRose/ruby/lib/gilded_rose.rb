246bcab1636192fb7f6005dd6a4c6df64e09a5dd
TomStuart92 <Thomascstuart@gmail.com>
2016-10-19 12:24:52 +0100

    Add Quality Methods

--- GildedRose/ruby/lib/gilded_rose.rb
+++ GildedRose/ruby/lib/gilded_rose.rb
@@ -1,68 +1,43 @@
+require_relative 'item'
+
 class GildedRose
 
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+  def update_quality
+    updated_items = seperate_items.each do |key, value|
+      value.each do |item|
+        rate = get_rate(item)
+        update_item(item, rate)
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
+    @items = updated_items.values.flatten
   end
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+  private
+
+  def seperate_items
+    @items.group_by(&:name)
   end
+
+  def update_item(item, rate)
+    item.sell_in -= rate[:sell_in]
+    item.quality -= rate[:quality]
+  end
+
+  def get_rate(item)
+    item.name
+
+
+    return {sell_in: 0, quality: 0 } if item.sell_in <= 0
+    return {sell_in: 1, quality: 0 } if item.quality >= 50
+    rates = {"Aged Brie" => {sell_in: 1, quality: -1 },
+     "Sulfuras, Hand of Ragnaros" => {sell_in: 0, quality: 0 },
+     "Backstage passes to a TAFKAL80ETC concert" => {sell_in: 1, quality: -(1 + (10/item.sell_in)) }}
+    other = {sell_in: 1, quality: 1 }
+    rates(item).include?(item.name) ? rates[item.name] : other
+  end
+
 end
--- GildedRose/ruby/spec/gilded_rose_spec.rb
+++ GildedRose/ruby/spec/gilded_rose_spec.rb
@@ -1,13 +1,15 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require './lib/gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
-    end
+  let(:aged_brie) {double :item, name: "Aged Brie"}
+  let(:dexterity_vest) {double :item, name: "Dexterity Vest"}
+
+  subject(:gilded_rose) {described_class.new([aged_brie, dexterity_vest, aged_brie])}
+
+  it "seperates_items" do
+    expect(gilded_rose.updated_items).to eq({"Aged Brie"=> [aged_brie, aged_brie], "Dexterity Vest"=>[dexterity_vest]})
   end
+
 
 end
